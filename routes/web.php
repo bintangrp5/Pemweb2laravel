b651fe93173f755213a12a3b949738d10513a28f
bintangrp5 <bintangrafli73@gmail.com>
2025-05-12 07:01:57 +0700

    praktikum5

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,40 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductCategoryController;
+use App\Http\Controllers\ProductController;
 
-Route::get('/',[HomepageController::class,'index']);
-Route::get('products', [HomepageController::class, 'products']);
+
+
+Route::get('/',[HomepageController::class,'index'])->name('home');
+Route::get('products', [ProductController::class, 'products']);
 Route::get('product/{slug}', [HomepageController::class, 'product']);
 Route::get('categories',[HomepageController::class, 'categories']);
 Route::get('category/{slug}', [HomepageController::class, 'category']);
 Route::get('cart', [HomepageController::class, 'cart']);
 Route::get('checkout', [HomepageController::class, 'checkout']);
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+//membuat prefix url untuk admin, dengan prefix dashboard
+Route::group(['prefix'=>'dashboard'], function(){
+    //kode nya disini
+    Route::resource('categories',ProductCategoryController::class);
+});
+
+Route::prefix('/dashboard')->middleware(['auth'])->group(function () {
+    Route::resource('products', ProductController::class);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
 
 require __DIR__.'/auth.php';
